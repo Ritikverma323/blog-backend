4d36d08354e721c67aa97435cb486775a4cebeb2
Ritik verma <iritikverm7@gmail.com>
2025-10-10 15:33:17 +0530

    development: Fix test cases..

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockUserModel = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    // add other methods your service uses
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken('User'), // 🔑 Important
+          useValue: mockUserModel,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
